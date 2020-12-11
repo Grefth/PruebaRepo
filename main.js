c23bbcc917a62f4ef75732a1f6634f989988cb9d
Grefth <rumeles@live.com.mx>
2020-12-11 14:06:27 -0700

    My tarjet primer commit del proyecto

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hola mundo");
+var a = parseInt(prompt('Introduce el valor de a'))
+var b = parseInt(prompt('Introduce el valor de b'))
+//var c = parseInt(prompt('Introduce el valor de c'))
+
+
+
+if (!isNaN(a)  && !isNaN(b)) {
+    document.writeln(a + '<br>')
+    document.writeln(b + '<br>')
+    quienEsMayor(a,b)
+}else{
+    alert('NO INGRESASTE UN NUMERO BIEN intenta nuevamente')
+}
+
+function quienEsMayor(a, b){
+    if (a > b) {
+        document.writeln('El numero mayor es = ' + a)
+    } else if ( b > a) {
+        document.writeln('El numero mayor es = ' + b)
+    } else {
+        document.writeln('Los numeros son iguales no le hagas al cuento')
+    }
+    
+}
+
+
