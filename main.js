2dbdfb03c197e18fa11ecb8960347a4a49f9791b
Grefth <rumeles@live.com.mx>
2020-12-11 14:11:08 -0700

    Primer commit de MyTarjet

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-var a = parseInt(prompt('Introduce el valor de a'))
-var b = parseInt(prompt('Introduce el valor de b'))
-//var c = parseInt(prompt('Introduce el valor de c'))
+var modelo
+var color
+var numeroSerie
+var placa
 
-
-
-if (!isNaN(a)  && !isNaN(b)) {
-    document.writeln(a + '<br>')
-    document.writeln(b + '<br>')
-    quienEsMayor(a,b)
-}else{
-    alert('NO INGRESASTE UN NUMERO BIEN intenta nuevamente')
+function crearCarro(modelo, color, numeroSerie, placa) {
+    this.modelo = modelo;
+    this.color = color;
+    this.numeroSerie = numeroSerie
+    this.placa = placa
 }
 
-function quienEsMayor(a, b){
-    if (a > b) {
-        document.writeln('El numero mayor es = ' + a)
-    } else if ( b > a) {
-        document.writeln('El numero mayor es = ' + b)
-    } else {
-        document.writeln('Los numeros son iguales no le hagas al cuento')
-    }
-    
-}
+    var vento = new crearCarro('2018', 'blanco', 14280530, 'AWS-1228')
+    var jetta = new crearCarro('2019', 'gris', 14280531, 'JTA-1229')
+    var cacharrito = new crearCarro('2020', 'blanco', 14280532, 'MACH-1230')
 
 
+var carros = [
+    vento,
+    jetta,
+    cacharrito
+];
+
+console.log(carros[0]);
+
+
+
+
+$("#btnInicio").click(function(){
+    $("#texto").text("nuevo texto con JQUERY");
+});
